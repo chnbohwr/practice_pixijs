7bce682e9431532fde473cb2269831cec9022a02
chnbohwr <chnbohwr@gmail.com>
2019-09-06 13:22:52 +0800

    add practice load texture

---
+++ practice_01/index.js
@@ -0,0 +1,34 @@
+import * as PIXI from 'pixi.js';
+import Stats from 'stats.js';
+import { promiseLoad } from '../utils/index';
+
+const settingStats = () => {
+  const stats = new Stats();
+  stats.showPanel(0);
+  stats.dom.style.left = null;
+  stats.dom.style.right = 0;
+  document.body.appendChild(stats.dom);
+  const calc = () => { stats.begin(); stats.end(); requestAnimationFrame(calc); }
+  calc();
+}
+
+const initApp = async () => {
+  console.log('initApp');
+  const app = new PIXI.Application({
+    width: 600,
+    height: 600,
+    view: document.getElementById('pixi'),
+  });
+  window.app = app;
+  app.loader.add('cat', 'resources/cat.png');
+  await promiseLoad(app.loader);
+  const cat = new PIXI.Sprite(app.loader.resources.cat.texture);
+  app.stage.addChild(cat);
+}
+
+document.onreadystatechange = () => {
+  if (document.readyState == "complete") {
+    settingStats();
+    initApp();
+  }
+}
---
+++ practice_01/readme.md
@@ -0,0 +1,14 @@
+### 1. install parcel on global
+```bash
+npm i -g parcel-bundler
+```
+
+### 2. install project dependencies
+```bash
+npm i
+```
+
+### 3. start project
+```bash
+npm start
+```
